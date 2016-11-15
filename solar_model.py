90c23cd8b1faa2a44087d8ff6de29fb5c8290bb6
giorgio <playclevergames@mail.ru>
2016-11-15 10:04:42 +0300

    fix t -> dt

--- solar_model.py
+++ solar_model.py
@@ -7,41 +7,39 @@ gravitational_constant = 6.67408E-11
 
 def calculate_force(body, space_objects):
     """Вычисляет силу, действующую на тело.
-
     Параметры:
-
     **body** — тело, для которого нужно вычислить дейстующую силу.
     **space_objects** — список объектов, которые воздействуют на тело.
     """
 
     body.Fx = body.Fy = 0
     for obj in space_objects:
-        if body == obj:
-            continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        if body != obj:
+              # тело не действует гравитационной силой на само себя!
+            r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
+            F = gravitational_constant*body.m*obj.m/(r**2)
+            body.Fx += F*(body.x - obj.x)/r**(0.5) # FIXME: нужно вывести формулу...
+            body.Fy += F*(body.y - obj.y)/r**(0.5)  # FIXME: нужно вывести формулу...
 
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
-
     Параметры:
-
     **body** — тело, которое нужно переместить.
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    body.x += ax*(dt**2)/2  # FIXME: не понимаю как менять...
     body.Vx += ax*dt
+    ay = body.Fy/body.m
+    body.y += ay*(dt**2)/2
+    body.Vy += ay*dt
     # FIXME: not done recalculation of y coordinate!
 
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
-
     Параметры:
-
     **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
     **dt** — шаг по времени
     """
